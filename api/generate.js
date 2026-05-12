44eebbd48ac9769d734968dc3daca4b32a71c7cb
briankiseka532-collab <briankiseka532@gmail.com>
2026-05-12 23:07:25 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,24 @@
 export default async function handler(req, res) {
 
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  if(req.method === "OPTIONS"){
+    return res.status(200).end();
+  }
+
   if(req.method !== "POST"){
     return res.status(405).json({
       error:"Method not allowed"
@@ -15,7 +34,7 @@ export default async function handler(req, res) {
       {
         method: "POST",
         headers: {
-          Authorization: "Bearer hf_xxxxxxxxxxxxxxxxx",
+          Authorization: "Bearer YOUR_HF_TOKEN",
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
@@ -26,11 +45,11 @@ export default async function handler(req, res) {
 
     if (!response.ok) {
 
-      const errorText =
+      const txt =
         await response.text();
 
       return res.status(500).json({
-        error:errorText
+        error: txt
       });
 
     }
