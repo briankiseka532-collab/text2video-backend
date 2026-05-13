738e06323358c5f30ccc0f3589c50689d88e38ba
briankiseka532-collab <briankiseka532@gmail.com>
2026-05-13 07:56:33 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
       {
         method: "POST",
         headers: {
-          Authorization: "Bearer hf_...pTsl",
+          Authorization: "Bearer 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",
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
